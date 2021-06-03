5d95177a35f1f7a18dd83d185e00014505ea956c
rd1042 <rd1042@fusion213.its.york.ac.uk>
2021-06-03 10:05:24 +0100

    tools added for making beta scans

---
+++ postprocessing_tools/extract_sim_data.py
@@ -0,0 +1,141 @@
+""" """
+
+from helper_ncdf import view_ncdf_variables, extract_data_from_ncdf
+import numpy as np
+import sys
+
+def get_omega_data(sim_longname, sim_type="stella"):
+    """ """
+
+    if sim_type == "stella":
+        time, freqom_final, gammaom_final, freqom, gammaom = get_omega_data_stella(sim_longname)
+    elif sim_type == "gs2":
+        time, freqom_final, gammaom_final, freqom, gammaom = get_omega_data_gs2(sim_longname)
+    else:
+        print("sim_type not recognised!")
+
+    return time, freqom_final, gammaom_final, freqom, gammaom
+
+def get_omega_data_stella(sim_longname):
+    """The .omega file contains columns t, ky, kx, re(omega), im(omega), av re(omega), av im(omega).
+    (NB. Some .omega files contain NaN values)
+    Read this file, and put data in arrays. We also want to calculate
+    the growth rate from the phi2_vs_kxky entry in the .out.nc file."""
+    omega_filename = sim_longname + ".omega"
+    omega_file = open(omega_filename, "r")
+    omega_data=np.loadtxt(omega_filename,dtype='float')
+    omega_file.close()
+
+    # Find the number of unique kx, kx, and construct omega-related arrays with dims (kx.ky)
+    kx_array = np.array(sorted(set(omega_data[:,2])))
+    ky_array = np.array(sorted(set(omega_data[:,1])))
+    if ((len(kx_array) * len(ky_array)) > 1):
+        print("Multiple modes - not currently supproted!")
+        sys.exit()
+        freqom_kxky = np.zeros((len(kx_array), len(ky_array)))
+        gammaom_kxky = np.zeros((len(kx_array), len(ky_array)))
+        gamma_p2_kxky = np.zeros((len(kx_array), len(ky_array)))
+
+        # Extract t and phi2(t) from .out.nc
+        time, phi2_kxky = extract_data_from_ncdf((sim_longname + ".out.nc"), "t", "phi2_kxky")
+
+        # Loop through the lists, putting values into the arrays
+        # NB we assume that stella changes t, then ky, then kx when writing to file
+        print("(len(kx_array) * len(ky_array)) = ", (len(kx_array) * len(ky_array)))
+        for kx_idx in range(0, len(kx_array)):
+            for ky_idx in range(0, len(ky_array)):
+
+                # Find real(omega) and im(omega) for this kx, ky, ignoring nans and infs
+                # Also find the finite phi2 entries, and their corresponding t values.
+                mode_data = data[np.where((data[:,2] == kx_array[kx_idx]) & (data[:,1] == ky_array[ky_idx]))]
+                finite_mode_data = mode_data[np.where((np.isfinite(mode_data[:,3])) & (np.isfinite(mode_data[:,4])))]
+                mode_phi2 = phi2_kxky[:,kx_idx,ky_idx]
+                finite_mode_phi2_idxs = np.where(np.isfinite(mode_phi2))
+                fintite_mode_phi2 = mode_phi2[finite_mode_phi2_idxs]
+                finite_mode_t = time[finite_mode_phi2_idxs]
+
+                # If phi2 is too small, stella porbably has trouble fitting
+                # an expontential to it. Filter out low-phi2 values to avoid noise.
+                if fintite_mode_phi2[-1] < min_phi2:
+                    freqom_kxky[kx_idx, ky_idx] = 0
+                    gammaom_kxky[kx_idx, ky_idx] = 0
+                else:
+                    freqom_kxky[kx_idx, ky_idx] = finite_mode_data[-1,3]
+                    gammaom_kxky[kx_idx, ky_idx] = finite_mode_data[-1,4]
+
+                # Calculate growth rate based on phi2(t).
+                gamma_p2_kxky[kx_idx, ky_idx] = (1/(2*(finite_mode_t[-1]-finite_mode_t[0])) *
+                                    (np.log(fintite_mode_phi2[-1]) - np.log(fintite_mode_phi2[0])) )
+        return [kx_array, ky_array, freqom_kxky, gammaom_kxky, gamma_p2_kxky]
+    else:
+        #time, phi2 = extract_data_from_ncdf((sim_longname + ".out.nc"), "t", "phi2")
+        time = omega_data[:,0]
+        freqom_final = omega_data[-1, 3]
+        gammaom_final = omega_data[-1, 4]
+
+        return time, freqom_final, gammaom_final, omega_data[:,3], omega_data[:,4]
+
+def get_omega_data_gs2(sim_longname):
+    """ """
+
+    try:
+        time, omega = extract_data_from_ncdf((sim_longname + ".out.nc"), "t", "omega_average")
+    except KeyError:
+        time, omega = extract_data_from_ncdf((sim_longname + ".out.nc"), "t", "omegaavg")
+
+    ## Check that there's only 1 mode i.e. omega has dimensions (n_time, 1, 1)
+    if ((len(omega[0]) > 1) or (len(omega[0][0]) > 1)):
+        print("omega[0]= ", omega[0])
+        print("len(omega[0]) > 1 or len(omega[0][0]) > 1, aborting")
+        sys.exit()
+    omega = omega[:,0,0]
+    freqom_final = omega[-1].real
+    gammaom_final = omega[-1].imag
+    freq = omega.real
+    gamma = omega.imag
+
+
+
+    return time, freqom_final, gammaom_final, freq, gamma
+
+def get_phiz_data(sim_longname, sim_type="stella"):
+    """ """
+    if sim_type == "stella":
+        theta, real_phi, imag_phi = get_phiz_data_stella(sim_longname)
+    elif sim_type == "gs2":
+        theta, real_phi, imag_phi = get_phiz_data_gs2(sim_longname)
+    else:
+        print("sim_type not recognised!")
+
+    return theta, real_phi, imag_phi
+
+def get_phiz_data_stella(sim_longname):
+    """ """
+    final_fields_filename = sim_longname + ".final_fields"
+    final_fields_file = open(final_fields_filename, "r")
+    final_fields_data=np.loadtxt(final_fields_filename,dtype='float')
+    final_fields_file.close()
+
+    ## final_fields_data = z, z-zed0, aky, akx, real(phi), imag(phi), real(apar), imag(apar), z_eqarc-zed0, kperp2
+    # Usually we're just looking at one mode; check how many unique kx and ky we have
+
+    z = final_fields_data[:,0]
+
+    aky = final_fields_data[:,2]; akx = final_fields_data[:,3]
+    unique_aky = set(aky); unique_akx = set(akx)
+    if len(unique_aky) > 1 or len(unique_akx) > 1:
+        print("len(unique_aky), len(unique_akx) = ", len(unique_aky), len(unique_akx))
+        print("Not currently supported")
+        sys.exit()
+
+    real_phi = final_fields_data[:,4]; imag_phi = final_fields_data[:,5]
+    return z, real_phi, imag_phi
+
+def get_phiz_data_gs2(sim_longname):
+    """ """
+    theta, phi = extract_data_from_ncdf((sim_longname + ".out.nc"), "theta","phi")
+    if ((len(phi) > 1) or (len(phi[0]) > 1)):
+        print("phi= ", phi)
+        print("(len(phi) > 1) or (len(phi[0]) > 1), aborting")
+    phi = phi[0,0,:]
+    return theta, phi.real, phi.imag
---
+++ test_cbc_beta_scan/make_plots.py
@@ -0,0 +1,33 @@
+""" """
+
+import sys
+sys.path.append("../postprocessing_tools")
+from plotting_helper import make_beta_scan_plots
+
+
+
+if __name__ == "__main__":
+    ## Compare
+    stella_sim_longnames = [
+                            "stella_beta0.000/input",
+                            "stella_beta0.005/input",
+                            "stella_beta0.010/input",
+                            "stella_beta0.015/input",
+                            "stella_beta0.020/input",
+                            "stella_beta0.025/input",
+                            "stella_beta0.030/input",
+                            ]
+    stella_beta_vals = [
+                        0.0,
+                        0.005,
+                        0.01,
+                        0.015,
+                        0.02,
+                        0.025,
+                        0.03
+                        ]
+
+    make_beta_scan_plots(stella_sim_longnames,
+                         stella_beta_vals,
+                          "./test_cbc_beta_scan",
+                         )
