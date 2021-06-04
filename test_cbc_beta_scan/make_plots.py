b0e5e952acac93c2f331f0db8f2c4a5a8c4a2ffd
rd1042 <rd1042@fusion213.its.york.ac.uk>
2021-06-04 15:22:36 +0100

    Improved plotting for CBC scan with fbpar=0

--- test_cbc_beta_scan/make_plots.py
+++ test_cbc_beta_scan/make_plots.py
@@ -33,3 +33,28 @@ if __name__ == "__main__":
                           "./test_cbc_beta_scan",
                           gs2_pickle=gs2_pickle
                          )
+
+    stella_sim_longnames = [
+                            "stella_beta0.000/input",
+                            "stella_beta0.001_fbpar0/input",
+                            "stella_beta0.002_fbpar0/input",
+                            "stella_beta0.003_fbpar0/input",
+                            "stella_beta0.004_fbpar0/input",
+                            "stella_beta0.005_fbpar0/input",
+                            ]
+    stella_beta_vals = [
+                        0.0,
+                        0.001,
+                        0.002,
+                        0.003,
+                        0.004,
+                        0.005,
+                        ]
+
+    gs2_pickle = "gs2_beta_scan_fbpar0/omega_values.pickle"
+
+    make_beta_scan_plots(stella_sim_longnames,
+                         stella_beta_vals,
+                          "./test_cbc_beta_scan_fbpar0",
+                         gs2_pickle=gs2_pickle
+                         )
